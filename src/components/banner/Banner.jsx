b986f26df194b7fa23befd607f79ad39eed1ac42
Md. Shofiqul Islam <shofiqebr@gmail.com>
2023-12-16 01:55:41 +0600

    added skills

--- src/components/banner/Banner.jsx
+++ src/components/banner/Banner.jsx
@@ -1,20 +1,25 @@
 const Banner = () => {
   return (
     <div className="hero min-h-screen bg-base-200">
-      <div className="hero-content flex-col lg:flex-row-reverse">
+      <div className="hero-content flex-col lg:flex-row-reverse relative">
         <img
           src="images/profile.jpg"
-          className=" pl-60 h-80 rounded-lgS"
+          className=" lg:pl-60 h-80 rounded-lg "
           
         />
-        <img src="images/sideimage.jpg" alt="" className="w-40  lg:absolute top-20 right-20 z-50"/>
-        <button className="btn  absolute top-[450px] right-96 btn-accent">Frontend Developer</button>
+      <img src="images/sideimage.jpg" alt="" className="w-40 md:w-40 lg:w-40 hidden sm:hidden md:block lg:absolute lg:-top-20 lg:-right-20 z-50"/>
+
+      <button className="btn lg:absolute top-80 right-44 btn-accent">Frontend Developer</button>
+
 
         <div>
             <p>Hi I'm <br /></p>
           <h1 className="text-5xl font-bold">Md. Shofiqul Islam</h1>
           <p className="py-6">
-            I am a frontend react Developer. <br />
+            I am a frontend <b>react Developer</b>. <br />
+
+
+            <button className="btn btn-accent mt-10 "><a href="resume.pdf" download="resume.pdf" >download resume </a></button>
 
           </p>
         </div>
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,9 +1,11 @@
 import Banner from "../../components/banner/Banner";
+import Skills from "../../components/skills/Skills";
 
 const Home = () => {
     return (
         <div className="max-w-screen-xl mx-auto">
             <Banner></Banner>
+            <Skills></Skills>
         </div>
     );
 };
